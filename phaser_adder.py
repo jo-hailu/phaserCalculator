092a7738b77eea042da9984679b117135a7bf8b9
Yohans hailu <88979362+jo-hailu@users.noreply.github.com>
2021-08-17 11:30:53 +0300

    Update phaser_adder.py

--- phaser_adder.py
+++ phaser_adder.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python
 
 
-import math
-import cmath
+import math   ## used for pi cos() sin()
+import cmath  ## used for phase() to calculate phase from complex
+
+##
+##  this will make array of phasers from a string 
+##            if for example for 12<21+32<12
+##            the phase arry will be [ [12,21] , [32,0]  ]
+##            and if no degree is given it will add zero 
+##            sof for string 12<12+21
+##            ther phase array will be [ [12,21] ,[32,0] ]
 def makePhaserArray(phrStr):
        phArry = []
-       for i in phrStr.split("+"):
+       for i in phrStr.split("+"): 
                 spt = i.split("<")
-                if(len(spt)==2):
+                if(len(spt)==2): ## if the split by < has two parts
                         phArry.append(spt)
-                else:
+                else: ## this happes when there is zero phase .. so we add zero 
                         spt.append(0)
                         phArry.append(spt)
        return phArry
+## 
+## this function will add given the phase array discibed above 
+## it will add the phaser representations in complex form 
+##     it will return the sum in complex form 
 def addPhaserArray(Parray):
         allreal = 0;
         allimg  = 0;
@@ -26,15 +38,24 @@ def addPhaserArray(Parray):
                         allimg += mag*math.sin(deg)
         return complex(allreal,allimg)
 
+
+
+
+## this is where we get the string input 
 in1 = input(">")
 
-f =  addPhaserArray(makePhaserArray(in1));
-realS = "{:.4f}".format(f.real)
-imagS = "{:.4f}".format(f.imag)
+## the addphaser willl retrun it in complex format 
+zsum =  addPhaserArray(makePhaserArray(in1));
 
-mag = "{:.4f}".format(abs(f))
-phase = "{:.4f}".format(cmath.phase(f)*180/math.pi)
-
-
+## we print it in complex format 
+## it used string fromating to make it 4 decimal places
+realS = "{:.4f}".format(zsum.real)
+imagS = "{:.4f}".format(zsum.imag)
 print(realS,"+j",imagS)
+
+
+## we also print it in phaser format 
+## it used string fromating to make it 4 decimal places 
+mag = "{:.4f}".format(abs(zsum))
+phase = "{:.4f}".format(cmath.phase(zsum)*180/math.pi) 
 print(mag,"<",phase)
